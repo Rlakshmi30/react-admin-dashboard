dd47e5b625058da5d776b16071d44662eda40ff4
Rlakshmi30 <r.lakshmi0048@gmail.com>
2026-07-03 14:08:20 +0530

    Updated Campaign page

--- src/pages/Campaign.tsx
+++ src/pages/Campaign.tsx
@@ -2,7 +2,7 @@ export default function Campaign() {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold">
-        lalalalalalaala
+        lalalalalalal
       </h1>
     </div>
   );
